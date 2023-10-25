a0da6c90655c1411b569addd00c110f8314a62d9
Ashutosh Jha <ashutoshj887@gmail.com>
2023-10-25 09:26:08 +0530

    init: Hacktober 2023

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center h-screen">
+      Welcome to Hacktober Fest 2023 Playground!
+    </main>
+  );
+}
