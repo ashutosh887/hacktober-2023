76541721e36db270f4e2e9e31ae744fc61935a65
Gilliash <130952207+Gilliash@users.noreply.github.com>
2023-11-05 00:29:37 +0700

    Add icon button for Mail and Github with Github username on Github icon button. (#63)
    * Add icon button for Mail and Github with Github username on it.
    
    * Add link to Github icon buttons.
    
    * Change to Link component
    
    * Use LinkButton component
    
    * Add link for email
    
    * Add real email into link

--- src/components/common/UserCard.tsx
+++ src/components/common/UserCard.tsx
@@ -1,5 +1,8 @@
 import { extractUserName } from "@/util/extractUserName";
 import React, { useEffect, useState } from "react";
+import { AiOutlineMail, AiFillGithub } from "react-icons/ai";
+import Link from "next/link";
+import LinkButton from "./LinkButton";
 
 type Props = {
   name?: string;
@@ -20,9 +23,21 @@ function UserCard({ name, email, githubURL, about }: Props) {
   return (
     <div className="flex flex-col bg-secondary rounded-lg p-4">
       <span className="text-primary text-xl">{name || "undefined"}</span>
-      <span>{email || "undefined"}</span>
-      <span>{userName || "undefined"}</span>
       <span>{about || "undefined"}</span>
+
+      <LinkButton
+        text={email || "undefined"}
+        link={`mailto:${email}`}
+        targetBlank={true}
+        icon={<AiOutlineMail className="inline-block w-6 h-6 mr-2" />}
+      />
+
+      <LinkButton
+        text={userName || "undefined"}
+        link={`https://github.com/${userName}`}
+        targetBlank={true}
+        icon={<AiFillGithub className="inline-block w-6 h-6 mr-2" />}
+      />
     </div>
   );
 }
