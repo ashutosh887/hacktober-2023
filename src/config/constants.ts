89ff6d0be1843b519b4baa79def7fd21db844b7e
Ashutosh Jha <ashutoshj887@gmail.com>
2023-12-08 09:01:54 +0530

    update: constants

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,7 @@
 export const appName = "Hacktober Fest 2023";
 
-export const githubUsername = "ashutosh887"
+export const githubUsername = "ashutosh887";
 
-export const githubUrl = "https://github.com/ashutosh887"
+export const githubUrl = "https://github.com/ashutosh887";
+
+export const repositoryUrl = "https://github.com/ashutosh887/hacktober-2023";
